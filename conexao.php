6bf8a5b78f9ea0361fc6311f0a0a18e2c9c4c8f8
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2021-08-23 17:03:06 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,26 +6,26 @@
 
 //TREINAMENTO
 
-$dbstr1 ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =192.168.90.231)(PORT = 1521))
-(CONNECT_DATA = (SID = trnmv)))";
+//$dbstr1 ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =192.168.90.231)(PORT = 1521))
+//(CONNECT_DATA = (SID = trnmv)))";
 
 
 ////Criar a conexao ORACLE
-if(!@($conn_ora = oci_connect('dbamv','treinamento123',$dbstr1,'AL32UTF8'))){
+//if(!@($conn_ora = oci_connect('dbamv','treinamento123',$dbstr1,'AL32UTF8'))){
+//	echo "Conexão falhou!";	
+//} else { 
+	//echo "Conexão OK!";	
+//}
+
+//PRODUCAO
+
+$dbstr1 ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =10.200.0.211)(PORT = 1521))
+(CONNECT_DATA = (SERVICE_NAME = prdmv)))";
+
+
+//Criar a conexao ORACLE
+if(!@($conn_ora = oci_connect('PGRME','tbpkrag#2021',$dbstr1,'AL32UTF8'))){
 	echo "Conexão falhou!";	
 } else { 
 	//echo "Conexão OK!";	
 }
-
-//PRODUCAO
-
-//$dbstr1 ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =10.200.0.211)(PORT = 1521))
-//(CONNECT_DATA = (SERVICE_NAME = prdmv)))";
-
-
-//Criar a conexao ORACLE
-//if(!@($conn_ora = oci_connect('PGRME','tbpkrag#2021',$dbstr1,'AL32UTF8'))){
-	//echo "Conexão falhou!";	
-//} else { 
-	//echo "Conexão OK!";	
-//}
